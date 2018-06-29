4259a09a43751f5caca69fcaee29ae38a841eb30
bishoy <bishoyaziz93i@gmail.com>
2018-06-29 18:02:11 +0200

    added files

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    // Read more button
+    $('#read-btn').click(function () {
+        if ($('.visibility').is(":visible")) {
+            $('#read-btn').text("Read More");
+        } else {
+            $('#read-btn').text("Show Less");
+        }
+        $('.visibility').toggle(600);
+    });
+
+    // Adjust header height
+    var myHeader = $('.header');
+
+    myHeader.height($(window).height());
+    $(window).resize(function () {
+        myHeader.height($(window).height());
+    });
+
+    // Add active to navbar link 
+    $('.links').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    //Navbar change background on scroll
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $(".navbar-fixed-top").css("background-color", "rgb(76, 84, 98)");
+        } else {
+            $(".navbar-fixed-top").css("background-color", "transparent");
+        }
+    });
+
+    // Type Writer
+    $(".type").typed({
+        strings: ["Welcome To JiosDev"],
+        typeSpeed: 100,
+        backDelay: 800,
+        loop: false,
+        showCursor: true,
+        cursorChar: '|',
+        autoInsertCss: true,
+    });
+    // Testimonials
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000,
+    });
+
+});
+
+// Nicescroll and remove horizontal scroll
+$(function () {
+    'use strict';
+    $('body').niceScroll({
+        horizrailenabled: false
+    });
+});
